46e008b25ee3fcf53cef7934062e0ac0e732afbf
Rahul <Rahul@Rahulsingh>
2021-05-11 21:40:37 +0530

    Initial stage for discovery server.

---
+++ mca-discovery/src/test/java/com/ipru/mca/mcadiscovery/McaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipru.mca.mcadiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McaDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
